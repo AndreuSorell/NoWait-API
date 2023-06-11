ea3f1349e6924df771cc1033b20c713066dcdf59
Andreu Sorell Pou <91556405+AndreuSorell@users.noreply.github.com>
2023-06-11 12:16:58 +0200

    feat: Return Comments of last 8 hours

--- src/core/main/edu/poniperro/nowait/core/comment/comment/application/search/CommentsSearcher.java
+++ src/core/main/edu/poniperro/nowait/core/comment/comment/application/search/CommentsSearcher.java
@@ -5,6 +5,7 @@ import edu.poniperro.nowait.core.comment.comment.application.CommentResponse;
 import edu.poniperro.nowait.core.comment.comment.domain.CommentRepository;
 import edu.poniperro.nowait.shared.domain.Service;
 
+import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,13 +18,21 @@ public final class CommentsSearcher {
     }
 
     public CommentsResponse search(String placeId) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime limitDateTime = currentDateTime.minusHours(8); // Mostramos los comentarios de las últimas 8 horas
         return new CommentsResponse(
                 repository.searchByPlaceId(placeId)
                         .stream()
+                        .filter(comment -> {
+                            LocalDateTime commentDateTime = LocalDateTime.parse(comment.getCreationDate());
+                            return commentDateTime.isAfter(limitDateTime) && commentDateTime.isBefore(currentDateTime);
+                        })
                         .map(CommentResponse::fromAggregate)
                         .collect(Collectors.toList())
         );
     }
+
+
 
     public CommentsResponse searchByEmail(String email) {
         return new CommentsResponse(
